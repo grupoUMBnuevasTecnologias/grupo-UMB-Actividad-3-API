a61c5daf4a992354cb1f003402f5aa82bb837621
cesarmana <cesarmana@gmail.com>
2018-02-16 19:30:31 -0500

    creacion del index php
    Creacion del index php con una imagen incial

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <title>html</title>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <div align="center">
+        <img src="/grupo-UMB-Actividad-3-API/imagenes/grupo.jpg" style="max-width: 100%; width: auto; height: auto" />
+        <p><h1>Grupo UMB<br />Actividad 3: Interfaz de Programacion de Aplicacion (APIs)</h1></p>
+        </div>
+    </body>
+</html>
+
+<?php
+
+/**
+  * @copyright 2018
+ */
+?>
