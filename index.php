1cd1c841fe18e5c18af75d12b7d5f99092f19b76
cesarmana <cesarmana@gmail.com>
2018-02-19 17:11:37 -0500

    Pagina de Aspectos generales de las APIs
    Pagina de Aspectos generales de las APIs

--- index.php
+++ index.php
@@ -13,10 +13,12 @@
         <img src="imagenes/grupo.jpg" style="max-width: 100%; width: auto; height: auto"/>
         <p><h1>Grupo UMB<br />Actividad 3: Interfaz de Programacion de Aplicacion (APIs)</h1></p>
         </div>
+        <input type="button" value="API AspectosGenerales" onclick="window.location='aspectosGeneralesAPI.html'"/>
         <input type="button" value="Ejemplo API Translation GOOGLE" onclick="window.location='ejemplo-API-TRANSLATION-GOOGLE.html'"/>
         </div>
     </body>
 </html>
+
 
 <?php
 
