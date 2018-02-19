568b11da000d4a7b538912255728e8e9f862b44c
cesarmana <cesarmana@gmail.com>
2018-02-19 08:39:10 -0500

    Adicion de la pagina ejemplo-API-TRANSLATION-GOOGLE.html
    Adicion de la pagina ejemplo-API-TRANSLATION-GOOGLE.html

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
+        <div>
         <div align="center">
         <img src="imagenes/grupo.jpg" style="max-width: 100%; width: auto; height: auto"/>
         <p><h1>Grupo UMB<br />Actividad 3: Interfaz de Programacion de Aplicacion (APIs)</h1></p>
+        </div>
+        <input type="button" value="Ejemplo API Translation GOOGLE" onclick="window.location='ejemplo-API-TRANSLATION-GOOGLE.html'"/>
         </div>
     </body>
 </html>
